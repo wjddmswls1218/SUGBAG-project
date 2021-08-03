627d987a64059f989d99fa431aa52001d1ed3f6b
Eun Jin <ouhouh2222@gmail.com>
2021-08-03 18:19:59 +0900

    app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import path from "path";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan(`dev`));
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.listen(PORT, () => {
+  console.log(`${PORT} start `);
+});
