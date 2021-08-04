219ec7cff6770d6950684831b8f7cc959445102d
Eun Jin <ouhouh2222@gmail.com>
2021-08-04 22:32:00 +0900

    error

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,3 +1,9 @@
-export const testController = (req, res) => {
+export const testController = async (req, res) => {
+  const {
+    query: { searchValue },
+  } = req;
+
+  console.log(searchValue);
+
   res.render("base");
 };
