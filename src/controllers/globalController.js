0ead5ed13ae2b0ce2dd43915ef26c4587803bda3
Eun Jin <ouhouh2222@gmail.com>
2021-08-13 03:16:39 +0900

    a

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,9 +1,37 @@
-export const testController = async (req, res) => {
+export const mainController = async (req, res) => {
   const {
     query: { searchValue },
   } = req;
 
   console.log(searchValue);
 
-  res.render("base");
+  res.render("main");
+};
+
+export const hotelController = (req, res) => {
+  res.render("hotel");
+};
+
+export const motelController = (req, res) => {
+  res.render("motel");
+};
+
+export const campingController = (req, res) => {
+  res.render("camping");
+};
+
+export const glampingController = (req, res) => {
+  res.render("glamping");
+};
+
+export const hanokController = (req, res) => {
+  res.render("hanok");
+};
+
+export const pensionController = (req, res) => {
+  res.render("pension");
+};
+
+export const resortController = (req, res) => {
+  res.render("resort");
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,26 @@
 import express from "express";
 
-import { testController } from "../controllers/globalController";
+import {
+  mainController,
+  hotelController,
+  motelController,
+  campingController,
+  glampingController,
+  hanokController,
+  pensionController,
+  resortController,
+} from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", testController);
+globalRouter.get("/", mainController);
+
+globalRouter.get("/hotel", hotelController);
+globalRouter.get("/motel", motelController);
+globalRouter.get("/camping", campingController);
+globalRouter.get("/glamping", glampingController);
+globalRouter.get("/hanok", hanokController);
+globalRouter.get("/pension", pensionController);
+globalRouter.get("/resort", resortController);
 
 export default globalRouter;
